ff87a48a4ea2fc7eb1d3c3e19afcdebd0c1bd092
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2020-04-08 17:36:17 -0400

    Adds --noversion option to remove version from accession.version

--- fasta-ncbi-idfetch-name-long-to-short.pl
+++ fasta-ncbi-idfetch-name-long-to-short.pl
@@ -5,14 +5,19 @@ use strict;
 use Getopt::Long;
 
 my $usage;
-$usage  = "perl fasta-ncbi-idfetch-name-long-to-short.pl <fasta file>\n";
+$usage  = "perl fasta-ncbi-idfetch-name-long-to-short.pl [options] <fasta file>\n";
+$usage .= "\tOPTIONS:\n";
+$usage .= "\t\t--noversion: remove version from accession.version\n";
+
+my $do_noversion = 0; # 1 to remove version from accession.version
+&GetOptions( "noversion" => \$do_noversion);
 
 while(my $line = <>) { 
   if($line =~ /^>(\S+)(\s*.*)/) { 
     chomp $line;
     my ($name, $desc) = ($1, $2);
-    if($name =~ /^gi\|\d+\|\S+\|(\S+\.\d+)\|.*/) { 
-      $name = $1;
+    if($name =~ /^gi\|\d+\|\S+\|(\S+)\.(\d+)\|.*/) { 
+      $name = ($do_noversion) ? $1 : $1 . "." . $2;
     }
     printf ">%s%s\n", $name, $desc;
   }
